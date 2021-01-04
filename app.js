b28e76f54f7c62261650245f817a83235bf684ee
Tommy Ngo <tommykhoango@live.com>
2021-01-04 07:29:07 -0500

    Finished

---
+++ app.js
@@ -0,0 +1,38 @@
+const bars = document.querySelector("#bars");
+const menu = document.querySelector("#menu");
+
+bars.addEventListener("click", () => {
+  if (menu.classList.contains("hidden") && window.innerWidth < 768) {
+    console.log("Opened");
+    menu.classList.remove("hidden");
+    console.log(menu.classList);
+    menu.classList.add(
+      "flex",
+      "flex-col",
+      "text-center",
+      "bg-green-500",
+      "w-full",
+      "absolute",
+      "top-16"
+    );
+  } else {
+    console.log("Closed");
+    menu.classList.add("hidden");
+    console.log(menu.classList);
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    menu.classList.add("hidden");
+    menu.classList.remove(
+      "flex",
+      "flex-col",
+      "text-center",
+      "bg-green-500",
+      "w-full",
+      "absolute",
+      "top-16"
+    );
+  }
+});
